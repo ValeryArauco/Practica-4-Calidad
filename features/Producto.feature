3ea40f6d052c02df780182742dd4246aa88c0b44
Elian <eliantito234@gmail.com>
2024-12-01 18:58:47 -0400

    agregado Feature de Test para un Producto + 1 escenario

---
+++ features/Producto.feature
@@ -0,0 +1,11 @@
+Feature: Como usuario
+    Quiero ver el detalle de un articulo
+    disponible en la lista de articulos
+
+Background:
+    Given Estoy en el sitio de GMO online
+    And hago click en el boton "Enter GMO Online"
+
+Scenario: Ver un producto disponible
+    When hago click en "Padded Socks"
+    Then observo el detalle del producto "Padded Socks"
---
+++ features/step_definitions/TestProducto.rb
@@ -0,0 +1,38 @@
+Given(/^hago click en el boton "Enter GMO Online"$/) do
+    click_button("bSubmit")
+end
+
+When(/^hago click en "([^"]*)"$/) do |producto|
+    xpath_base = "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[5]/td[2]/a/strong"
+    find(:xpath, xpath_base).click
+end
+
+Then(/^observo el detalle del producto "([^"]*)"$/) do |producto| 
+    # Verificar el título
+  expect(page).to have_selector('a[name="socks"]', text: producto)
+
+  # Verificar la descripción
+  descripcion = find("body > p:nth-child(34)")
+  expect(page).to have_content(descripcion.text)
+
+  # Verificar la tabla de detalles
+  within("body > div:nth-child(35)") do
+    within('table') do
+        # Verificar el precio unitario
+        expect(page).to have_selector('td', text: 'Unit Price:')
+        expect(page).to have_selector('td', text: '$ 19.99')
+    
+        # Verificar la cantidad en stock
+        expect(page).to have_selector('td', text: '# In Stock:')
+        expect(page).to have_selector('td', text: '47')
+    
+        # Verificar el número del artículo
+        expect(page).to have_selector('td', text: 'Item Number:')
+        expect(page).to have_selector('td', text: '1003')
+        end
+    end
+  
+
+  # Verificar el texto de descripción adicional
+  expect(page).to have_content('These heavyweight socks are designed for long distances.')
+end
