4ef71f0c7e32ea905ff6f015daaf021a832dba9e
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2024-12-03 00:53:08 -0400

    Adicion escenarios feature recibo

--- features/8Receipt.feature
+++ features/8Receipt.feature
@@ -9,7 +9,7 @@ Background:
     And ingreso cantidades validas a ordenar para los items
     | item                     | cantidad |
     | Glacier Sun Glasses      | 1        |
-    | Hiking Boots             | 1        |
+    | Hiking Boots             | 2        |
 
     And hago click sobre el boton "Place An Order"
     And hago click sobre el boton "Proceed With Order"
@@ -32,15 +32,22 @@ Background:
 
     
 Scenario: Validar la información ingresada
-    Then debería mostrarse que los datos coincidan con los ingresados
+    Then debería mostrarse que los datos de facturacion y envio coincidan con los ingresados
     |Name       | Pepito             |
     |Address 	| Muyurina           |
     |City 	    | Cochabamba         |
     |State      | Cochabamba         | 	
     |Zip        | 33125              |
     |Phone   	| 123-123-1234       |
-    |E-mail  	| pepe@pepazo.com    |
-    |Credit Card| American Express  |
-    |Card Number| 1234-123456-12345 |
-    |Expiration	| 11/25 |
+    |Method     | AMEX               |
 
+    And deberia mostrar los productos seleccionados
+    | Qty | Product                  | Status        | Unit Price | Total Price |
+    | 1   | Glacier Sun Glasses      | To Be Shipped | $ 67.99	  | $ 67.99	    |
+    | 2   | Hiking Boots             | To Be Shipped | $ 109.90   | $ 219.80    |
+
+    And los detalles de precios deberian ser
+    | Product Total         | $ 287.79 |
+    | Sales Tax             | $ 14.39  |
+    | Shipping & Handling   | $ 5.00   |
+    | Grand Total           | $ 307.18 |
--- features/step_definitions/8Receipt.rb
+++ features/step_definitions/8Receipt.rb
@@ -1,8 +1,61 @@
-Then(/^debería mostrarse que los datos coincidan con los ingresados"$/) do |table|
-    expect(page).to have_selector('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(2) > strong', text: nombre)
+Then(/^debería mostrarse que los datos de facturacion y envio coincidan con los ingresados$/) do |table|
+    formulario = table.rows_hash
+
+    #Validacion datos de la factura
+    casillaName = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(2)').text
+    casillaAddress = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(2)').text
+    casillaCity = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(3) > td:nth-child(2)').text
+
+
+    
+    expect(casillaName).to eq(formulario["Name"])
+    expect(casillaAddress).to eq(formulario["Address"])
+    expect(casillaCity).to eq(formulario["City"]+", "+formulario["State"]+" "+formulario["Zip"])
+
+
+    #Validacion datos de envio
+    casillaName = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(5)').text
+    casillaAddress = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(5)').text
+    casillaCity = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(3) > td:nth-child(5)').text
+    casillaPhone = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(4) > td:nth-child(5)').text
+
+    
+    expect(casillaName).to eq(formulario["Name"])
+    expect(casillaAddress).to eq(formulario["Address"])
+    expect(casillaCity).to eq(formulario["City"]+", "+formulario["State"]+" "+formulario["Zip"])
+    expect(casillaPhone).to eq(formulario["Phone"])
+
+    #Validacion metodo de pago
+    casillaTarjeta = find('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(4) > td:nth-child(1) > strong').text
+
+    expect(casillaTarjeta).to eq(formulario["Method"])
 end
 
-And(/^debería mostrarse la dirección de facturación con direccion "(.*)" ciudad, estado y codigo zip "(.*)"$/) do |direccion, ciudad|
-    expect(page).to have_selector('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(2) > strong', text: nombre)
-    expect(page).to have_selector('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(2) > strong', text: nombre)
+And(/^deberia mostrar los productos seleccionados$/) do |table|
+    productos = table.hashes
+    productos.each do |producto|
+        within('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table > tbody') do
+            fila = find('tr', text: producto['Product'])
+            expect(fila).to have_text(producto['Qty'])
+            expect(fila).to have_text(producto['Status'])
+            expect(fila).to have_text(producto['Unit Price'])
+            expect(fila).to have_text(producto['Total Price'])
+        end
+    end
+end
+
+
+And('los detalles de precios deberian ser') do |table|
+    detalles = table.rows_hash
+    productoTotal = find('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(4) > td:nth-child(3)').text
+    salesTax = find('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(5) > td:nth-child(2)').text
+    shipping = find('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(6) > td:nth-child(2)').text
+    total = find('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(7) > td:nth-child(2)').text
+
+    
+    expect(productoTotal).to eq(detalles["Product Total"])
+    expect(salesTax).to eq(detalles["Sales Tax"])
+    expect(shipping).to eq(detalles["Shipping & Handling"])
+    expect(total).to eq(detalles["Grand Total"])
+   
 end
