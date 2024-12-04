e6fb8fa6f72d68dae02282b05e1f8d5a35636b75
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2024-12-04 15:29:45 -0400

    refactoring feature recibo selector

--- features/step_definitions/8Receipt.rb
+++ features/step_definitions/8Receipt.rb
@@ -1,41 +1,36 @@
 Then(/^debería mostrarse que los datos de facturacion y envio coincidan con los ingresados$/) do |table|
     formulario = table.rows_hash
 
-    #Validacion datos de la factura
-    casillaName = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(2)').text
-    casillaAddress = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(2)').text
-    casillaCity = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(3) > td:nth-child(2)').text
+    within('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody') do
+        #Validacion datos de la factura
+        casillaName = find('tr:nth-child(1) > td:nth-child(2)').text
+        casillaAddress = find('tr:nth-child(2) > td:nth-child(2)').text
+        casillaCity = find('tr:nth-child(3) > td:nth-child(2)').text
 
+        expect(casillaName).to eq(formulario["Name"])
+        expect(casillaAddress).to eq(formulario["Address"])
+        expect(casillaCity).to eq(formulario["City"]+", "+formulario["State"]+" "+formulario["Zip"])
 
-    
-    expect(casillaName).to eq(formulario["Name"])
-    expect(casillaAddress).to eq(formulario["Address"])
-    expect(casillaCity).to eq(formulario["City"]+", "+formulario["State"]+" "+formulario["Zip"])
+        #Validacion datos de envio
+        casillaName = find('tr:nth-child(1) > td:nth-child(5)').text
+        casillaAddress = find('tr:nth-child(2) > td:nth-child(5)').text
+        casillaCity = find('tr:nth-child(3) > td:nth-child(5)').text
+        casillaPhone = find('tr:nth-child(4) > td:nth-child(5)').text
 
-
-    #Validacion datos de envio
-    casillaName = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(1) > td:nth-child(5)').text
-    casillaAddress = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(5)').text
-    casillaCity = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(3) > td:nth-child(5)').text
-    casillaPhone = find('body > table:nth-child(5) > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-child(4) > td:nth-child(5)').text
-
-    
-    expect(casillaName).to eq(formulario["Name"])
-    expect(casillaAddress).to eq(formulario["Address"])
-    expect(casillaCity).to eq(formulario["City"]+", "+formulario["State"]+" "+formulario["Zip"])
-    expect(casillaPhone).to eq(formulario["Phone"])
-
-    within('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table') do
-        casillaTarjeta = find('tr', text: "Method of Payment:").find('strong:nth-child(1)').text
-    
-        expect(casillaTarjeta).to eq(detalles["Method"])
+        expect(casillaName).to eq(formulario["Name"])
+        expect(casillaAddress).to eq(formulario["Address"])
+        expect(casillaCity).to eq(formulario["City"]+", "+formulario["State"]+" "+formulario["Zip"])
+        expect(casillaPhone).to eq(formulario["Phone"])
     end
 
     #Validacion metodo de pago
-    casillaTarjeta = find('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table > tbody > tr:nth-child(4) > td:nth-child(1) > strong').text
-
-    expect(casillaTarjeta).to eq(formulario["Method"])
+    within('body > table:nth-child(5) > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > center > table') do
+        casillaTarjeta = find('tr', text: "Method of Payment:").find('strong:nth-child(1)').text
+    
+        expect(casillaTarjeta).to eq(formulario["Method"])
+    end
 end
+
 
 And(/^deberia mostrar los productos seleccionados$/) do |table|
     productos = table.hashes
