ee3985e92d737d5b2234270dbd6644865f8a8371
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2024-12-11 16:29:31 -0400

    Añadido de más pruebas a email y pedido features

--- features/1EmailTo.feature
+++ features/1EmailTo.feature
@@ -6,6 +6,10 @@ Feature:
 Background:
     Given Estoy en el sitio de GMO online
 
-Scenario: Enlace de correo es funcional
+Scenario: Enlace de correo es válido
+    When hago click en el enlace "Please let us know how you like our site."
+    Then la dirección de correo proporcionada debe ser válida
+
+Scenario: Enlace de correo es de gmo
     When hago click en el enlace "Please let us know how you like our site."
     Then el enlace me redirige a un correo "mailto:gmo-master@segue.com"
--- features/step_definitions/1EmailToSteps.rb
+++ features/step_definitions/1EmailToSteps.rb
@@ -2,8 +2,19 @@ When(/^hago click en el enlace "(.*)"$/) do |link_text|
     @clicked_link = find_link(link_text)[:href]
 end
   
+def validate_email(email)
+    email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    email.match?(email_regex)
+end
+
+Then(/^la dirección de correo proporcionada debe ser válida$/) do
+    correo = @clicked_link.gsub('mailto:', '')
+    expect(validate_email(correo)).to be_truthy, 
+        "El formato de correo no es válido"
+end
+
 Then(/^el enlace me redirige a un correo "(mailto:.*)"$/) do |expected_mailto|
     expect(@clicked_link).to start_with('mailto:') 
     expect(@clicked_link).to eq(expected_mailto)  
 end
-  
+
